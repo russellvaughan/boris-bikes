d5563b09e2053d29db6ec78c5ce7304f5b600ed9
Russell Vaughan <russellvaughan@gmail.com>
2016-01-19 16:26:10 +0000

    Revert "Day one"

--- spec/bike_spec.rb
+++
@@ -1,9 +0,0 @@
-require "bike"
-
-describe Bike do
-it "responds_to the working? method" do
-	expect(subject).to respond_to :working
-end
-end
-
-
--- spec/boris_bikes_spec.rb
+++
@@ -1,4 +0,0 @@
-require 'boris_bikes'
-describe DockingStation do
-  it { expect(subject).to respond_to :release_bike}
-end
