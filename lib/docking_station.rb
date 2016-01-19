9e5321efca9bd912795473c12ba54ab675ea4fc3
russellvaughan <russellvaughan@gmail.com>
2016-01-19 18:30:23 +0000

    challenge 10 complete

---
+++ lib/docking_station.rb
@@ -0,0 +1,7 @@
+require_relative 'bike'
+
+class DockingStation #title
+  def release_bike
+  	Bike.new
+  end
+end 
---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,14 @@
+require 'docking_station'
+
+
+describe DockingStation do
+  it { is_expected.to respond_to :release_bike}
+
+ it "releases working bike" do
+ bike = subject.release_bike 
+ expect(bike).to be_working
+end
+
+
+  #when we call 
+end
