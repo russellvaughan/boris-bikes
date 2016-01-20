de8333256cc46afa5e6ff7de51ca3ccac52f4f9c
BjoernWagner <bjoern.m.wagner@gmail.com>
2016-01-20 18:17:02 +0100

    docking bike method pass test

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,8 +1,8 @@
 require "bike"
+
 describe Bike do 
 	it "responds to the method working?" do
 		bike = Bike.new
 		expect(bike).to be_working
 	end
 end
-
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,5 +5,24 @@ describe DockingStation do
        # expect(subject).to respond_to(:release_bike)
        it { respond_to(:release_bike) }
 
- 
+    it "responds to the method dock_bike" do
+      expect(subject).to respond_to(:dock_bike)
+    end
+
+    it "responds to the method bikes_docked" do
+      expect(subject).to respond_to(:bikes_docked)
+    end
+
+    it "creates a new instance of bike when a bike is docked to the docking station" do
+      expect(subject.dock_bike).to be_a Bike
+    end
+
+    it "returns true if a bike has been docked" do
+      docking_station = DockingStation.new
+      newbike = docking_station.dock_bike
+      expect(docking_station.bikes_docked).to be_truthy
+    end
+       #it { respond_to(:dock_bike) }
+
+
 end
