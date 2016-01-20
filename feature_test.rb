695bf89ac88968ad31620c5bf6f04ae867abf61b
Rebecca Piper <piper.rebecca93@gmail.com>
2016-01-20 18:05:04 +0000

    revised feature_test.md

--- feature_test.rb
+++ feature_test.rb
@@ -2,6 +2,8 @@ require './lib/docking_station.rb'
 require './lib/bike.rb'
 
 station = DockingStation.new
+puts "station = #{station}"
 bike = Bike.new
-station.dock(bike)
-station.bike
+puts "bike = #{bike}"
+puts "station.dock(bike) = #{station.dock(bike)}"
+puts "station.bike = #{station.bike}"
