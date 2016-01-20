2e809149caa8d9b3cd705423797b0c9604cbf1fd
Douglas Rose <douglahr@gmail.com>
2016-01-20 17:17:28 +0000

    can dock bike test ex11

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,4 +6,12 @@ describe DockingStation do
 		bike = subject.release_bike
 		expect(bike).to be_working
 	end
+	
+	it "return bike has one argument" do
+		expect(subject).to respond_to(:dock).with(1).argument
+	end
+
+	it "shows if bike has been docked" do
+		expect(subject).to be_docked
+	end
 end
