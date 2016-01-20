6693e1093befbe1bda2a7b40e10b0e67856f0565
Iryna Howarth <irynahowarth@gmail.com>
2016-01-20 13:05:32 +0000

    Solved challange 11

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,10 @@
 require_relative 'bike'
 class DockingStation
+  attr_reader :bike
   def release_bike
     Bike.new
+  end
+  def dock bike
+    @bike = bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,27 @@
 require 'docking_station'
+
 describe DockingStation do
+
   it {is_expected.to respond_to :release_bike}
+
   it 'release working bike' do
     bike = subject.release_bike
     expect(bike).to be_working
+  end
+
+  it {is_expected.to respond_to(:dock).with(1).argument}
+
+  it 'dock bike' do
+    bike = subject.release_bike
+    dock = subject.dock(bike)
+    expect(dock).to eq (bike)
+  end
+
+  it {is_expected.to respond_to :bike}
+
+  it 'bike shows docked bikes' do
+    bike1 = subject.release_bike
+    dock = subject.dock(bike1)
+    expect(subject.bike).to eq dock
   end
 end
