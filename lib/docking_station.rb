5d868dbc7d040230435706f30521668d7adae601
Kait Beaudette <kait@kaitmakesthings.com>
2016-01-21 07:53:17 -0600

    Passed Challenge 14

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,12 +1,21 @@
 require_relative 'bike'
 class DockingStation
-  attr_reader :bike
+	attr_reader :bikes
+	def initialize
+		@bikes = []
+	end
+  
   def release_bike
-    raise "No bikes availiable error" unless @bike
-    @bike
+    raise "No bikes availiable error" unless @bikes.length > 0 
+    @bikes
   end
   def dock bike
-    raise "Docking Station is full" unless !@bike
-    @bike = bike
+    raise "Docking Station is full" if @bikes.length == 20
+    @bikes << bike
+   
   end
 end
+
+#docking_station = DockingStation.new
+#docking_station.dock(Bike.new)
+#docking_station.bikes
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -22,22 +22,26 @@ describe DockingStation do
     it 'dock bike' do
       bike = Bike.new
       dock = subject.dock(bike)
-      expect(dock).to eq (bike)
+      expect(dock).to eq [(bike)]
     end
 
     it 'raise error when docking station is full' do
-      dock = subject.dock(Bike.new)
-      bike = Bike.new
-      expect{subject.dock(bike)}.to raise_error("Docking Station is full")
+      20.times{subject.dock(Bike.new)}
+      expect{subject.dock(Bike.new)}.to raise_error("Docking Station is full")
     end
 
   end
 
-  it {is_expected.to respond_to :bike}
+  it {is_expected.to respond_to :bikes}
 
   it 'bike shows docked bikes' do
     bike1 = Bike.new
     dock = subject.dock(bike1)
-    expect(subject.bike).to eq dock
+    expect(subject.bikes).to eq dock
+  end
+
+  it 'can hold 20 bikes' do
+  20.times{subject.dock(Bike.new)}
+  expect(subject.bikes.length).to eq 20
   end
 end
