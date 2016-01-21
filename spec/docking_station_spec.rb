c347e2dfd47910319d782e193082ca4c3494b4af
russellvaughan <russellvaughan@gmail.com>
2016-01-21 09:42:57 +0000

    challenge 14 complete

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,10 @@
 require 'docking_station'
 require "bike.rb"
 
+
 describe DockingStation do
-    #it "responds to the method release_bike" do
-       # expect(subject).to respond_to(:release_bike)
-       it { respond_to(:release_bike) }
+ 
+    it { respond_to(:release_bike) }
 
     it "responds to the method dock" do
       expect(subject).to respond_to(:dock).with(1).argument
@@ -14,17 +14,10 @@ describe DockingStation do
       expect(subject).to respond_to(:bikes_docked)
     end
 
-
-    #it "creates a new instance of bike when a bike is docked to the docking station" do
-     # expect(subject.dock_bike).to be_a Bike
-    #end
-
-    #it "returns true if a bike has been docked" do
-      #docking_station = DockingStation.new
-      #newbike = docking_station.dock_bike
-     # expect(docking_station.bikes_docked).to be_truthy
-   # end
-       #it { respond_to(:dock_bike) }
+    it "returns true if a bike has been docked" do
+    (subject).dock(Bike.new)
+    expect(subject.bikes_docked).to be_truthy
+    end
 
       it "returns docked bikes" do
       bike =  Bike.new
