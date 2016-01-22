8e0d8092de7453152971de8a4630e2c0119dc898
russellvaughan <russellvaughan@gmail.com>
2016-01-22 11:44:44 +0000

    isolation tests pass

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,12 +1,13 @@
 require 'docking_station'
 
 describe DockingStation do
+let(:bike) { double :bike }
 
   describe '#release_bike' do
     it {is_expected.to respond_to :release_bike}
 
     it 'release working bike' do
-      bike = Bike.new
+      allow(bike).to receive(:working).and_return(true)
       expect(bike.working).to eq true
     end
 
@@ -15,30 +16,44 @@ describe DockingStation do
     end
 
     it 'doesnt release bike if broken' do
-      bike = Bike.new
+      allow(bike).to receive(:report).and_return(false)
+      allow(bike).to receive(:working).and_return(false)
       bike.report
       subject.dock(bike)
       expect{subject.release_bike}.to raise_error("No bikes availiable error")
     end
-  end
+  
+    it 'releases a working bike if multiple bikes are docked and at least one is broken' do
+    station = described_class.new
+    3.times { station.dock(bike)}
+    allow(bike).to receive(:working).and_return(true)
+    bike2 = double(:bike)
+    allow(bike2).to receive(:report).and_return(false)
+    bike2.report
+    station.dock(bike2)
+    allow(bike2).to receive(:working).and_return(false)
+    expect(station.release_bike.working).to eq true
+    end
 
+  end
+ 
   describe '#dock' do
 
     it {is_expected.to respond_to(:dock).with(1).argument}
 
     it 'dock bike' do
-      bike = Bike.new
       dock = subject.dock(bike)
       expect(dock).to eq [(bike)]
     end
 
     it 'raise error when docking station is full' do
-      DockingStation::DEFAULT_CAPACITY.times{subject.dock(Bike.new)}
-      expect{subject.dock(Bike.new)}.to raise_error("Docking Station is full")
+      DockingStation::DEFAULT_CAPACITY.times{subject.dock(bike)}
+      expect{subject.dock(bike)}.to raise_error("Docking Station is full")
     end
 
     it 'accepts broken bikes' do
-      bike = Bike.new
+      allow(bike).to receive(:report).and_return(false)
+      allow(bike).to receive(:working).and_return(false)
       bike.report
       expect(subject.dock(bike)).to eq [(bike)]
     end
@@ -48,13 +63,13 @@ describe DockingStation do
   it {is_expected.to respond_to :bikes}
 
   it 'bike shows docked bikes' do
-    bike1 = Bike.new
+    bike1 = bike
     dock = subject.dock(bike1)
     expect(subject.bikes).to eq dock
   end
 
   it 'can hold 20 bikes' do
-  DockingStation::DEFAULT_CAPACITY.times{subject.dock(Bike.new)}
+  DockingStation::DEFAULT_CAPACITY.times{subject.dock(bike)}
   expect(subject.bikes.length).to eq 20
   end
 
@@ -65,15 +80,7 @@ describe DockingStation do
   it 'can change capacity' do
     expect((DockingStation.new (50)).capacity).to eq 50
   end
-  
-  it 'releases a working bike if multiple bikes are docked and at least one is broken' do
-    station = described_class.new
-    3.times { station.dock(Bike.new)}
-    bike = Bike.new
-    bike.report
-    station.dock(bike)
-    expect(station.release_bike).to be_a Bike
-  end
+ 
+ end 
+    
 
-
-end
